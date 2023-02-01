8d2ddaac0e0b6787bdf3240250c3d8372c8e383d
MeeraNair07 <meera07nair@gmail.com>
2023-02-01 20:26:42 +0300

    looping over vowels and consonants

---
+++ Day 2: Loops.js
@@ -0,0 +1,16 @@
+/*
+ * Complete the vowelsAndConsonants function.
+ * Print your output using 'console.log()'.
+ */
+function vowelsAndConsonants(s) {
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
+            console.log(s[i])
+        }
+    }
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] != 'a' && s[i] != 'e' && s[i] != 'i' && s[i] != 'o' && s[i] != 'u') {
+            console.log(s[i])
+        }
+    }
+}
